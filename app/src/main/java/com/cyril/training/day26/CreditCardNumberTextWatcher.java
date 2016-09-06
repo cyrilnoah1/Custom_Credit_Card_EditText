9a123e0717ad176fcbb4bd73587cd13ca4b32179
cyrilnoah1 <cyril.noah1@gmail.com>
2016-09-06 12:37:58 +0530

    commit

---
+++ app/src/main/java/com/cyril/training/day26/CreditCardNumberTextWatcher.java
@@ -0,0 +1,105 @@
+package com.cyril.training.day26;
+
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+
+/**
+ * TextWatcher subclass to format the entered input (Credit Card number) into
+ * required format.
+ */
+public class CreditCardNumberTextWatcher implements TextWatcher
+{
+    private static final String LOG_TAG= "day26Log";
+
+    // Gap/Space between every four numbers in EditText.
+    private static final char numberGap= ' ';
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void afterTextChanged(Editable s)
+    {
+        if((s.length() > 0) && (s.length() % 5 == 0))
+        {
+            char ch= s.charAt(s.length() - 1);
+
+            if(Character.isDigit(ch) /*Checking if the current character is a digit*/
+                    && TextUtils.split(s.toString(), String.valueOf(numberGap)).length <= 3)
+            {
+                // Inserting gaps between
+                s.insert(s.length() - 1, String.valueOf(numberGap));
+                Log.v(LOG_TAG, "Gap inserted after four digits.");
+            }
+        }
+
+        // Detecting gaps after every four digits.
+        if((s.length() > 0) && (s.length() % 5 == 0))
+        {
+            char ch= s.charAt(s.length() - 1);
+
+            // Checking if the assigned character is a white-space.
+            if(ch == numberGap)
+            {
+                // Deleting the gap between FROM and TO
+                s.delete(s.length() - 1/*FROM*/, s.length()/*TO*/);
+
+                Log.v(LOG_TAG, "Gap removed between digits.");
+            }
+        }
+
+        // Adding gap between every four digits.
+//        addGapBetweenNumbers(inputText);
+
+        // Removing gap between every four digits.
+//        removeGapBetweenNumbers(inputText);
+    }
+
+    /**
+     * Method to add gap between numbers, when user gives input.
+     * @param textInput
+     */
+    private void addGapBetweenNumbers(Editable textInput)
+    {
+        // Placing gaps after every four digits
+        if((textInput.length() > 0) && (textInput.length() % 5 == 0))
+        {
+            char ch= textInput.charAt(textInput.length() - 1);
+
+            if(Character.isDigit(ch) /*Checking if the current character is a digit*/
+                && TextUtils.split(textInput.toString(), String.valueOf(numberGap)).length <= 3)
+            {
+                // Inserting gaps between
+                textInput.insert(textInput.length() - 1, String.valueOf(numberGap));
+                Log.v(LOG_TAG, "Gap inserted after four digits.");
+            }
+        }
+    }
+
+    /**
+     * Method to remove added gaps between numbers when user presses Back Button.
+     * @param textInput
+     */
+    private void removeGapBetweenNumbers(Editable textInput)
+    {
+        // Detecting gaps after every four digits.
+        if((textInput.length() > 0) && (textInput.length() % 5 == 0))
+        {
+            char ch= textInput.charAt(textInput.length() - 1);
+
+            // Checking if the assigned character is a white-space.
+            if(ch == numberGap)
+            {
+                // Deleting the gap between FROM and TO
+                textInput.delete(textInput.length() - 1/*FROM*/, textInput.length()/*TO*/);
+                
+                Log.v(LOG_TAG, "Gap removed between digits.");
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/cyril/training/day26/MainActivity.java
@@ -0,0 +1,71 @@
+package com.cyril.training.day26;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    /*
+    * IMPORTANT:
+    *
+    * Make sure to use "inputType='phone'" instead of "inputType='number'" as an
+    * attribute in EditText's View, in the Layout, in order to insert spaces between the input.
+    *
+    * */
+
+    private static final String LOG_TAG= "day26Log";
+
+    TextView mTextViewForCardNumber; // To show the Credit Card number on Submit.
+    EditText mEditTextForCardNumber; // To get and format the Credit Card number
+    Button mSubmitButton;// To submit and show the Credit Card number in the TextView.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewForCardNumber = (TextView) findViewById(R.id.textView_for_credit_card);
+        mEditTextForCardNumber = (EditText) findViewById(R.id.editText_for_Credit_card);
+
+        // Checking and adding gaps between every four numbers.
+        mEditTextForCardNumber.addTextChangedListener(new CreditCardNumberTextWatcher());
+
+        mSubmitButton = (Button) findViewById(R.id.button_to_submit_credit_card_number);
+
+        // Showing the Credit Card number in the TextView on Submit.
+        mSubmitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Checking for the correct format.
+                if((mEditTextForCardNumber.length() > 0) && (mEditTextForCardNumber.length() == 19))
+                {
+                    String cardNumber= mEditTextForCardNumber.getText().toString();
+                    mTextViewForCardNumber.setText(cardNumber);
+                }
+                else if ((mEditTextForCardNumber.length() == 0) && (mEditTextForCardNumber.length() < 19))
+                {
+                    Log.v(LOG_TAG, "EditText is blank.");
+                    Toast.makeText(MainActivity.this, "EditText is blank.", Toast.LENGTH_SHORT).show();
+                }
+                else if(mEditTextForCardNumber.length() < 19)
+                {
+                    Log.v(LOG_TAG, "Insert full Card number.");
+                    Toast.makeText(MainActivity.this, "Insert full Card number.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+
+}
